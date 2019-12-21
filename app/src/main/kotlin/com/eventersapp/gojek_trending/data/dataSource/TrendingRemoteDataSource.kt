db050ff32ea059992b5320d7037fb471c4a1a18b
reprator <mr.vikram004@gmail.com>
2019-12-21 17:25:50 +0530

    data folder for abstraction over domain layer added

---
+++ app/src/main/kotlin/com/eventersapp/gojek_trending/data/dataSource/TrendingRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.eventersapp.gojek_trending.data.dataSource
+
+import com.eventersapp.gojek_trending.domain.TrendingModal
+import com.eventersapp.gojek_trending.domain.baseUseCase.Result
+
+interface TrendingRemoteDataSource {
+    suspend fun trendingRepo(): Result<TrendingModal>
+
+}
---
+++ app/src/main/kotlin/com/eventersapp/gojek_trending/data/repository/TrendingRemoteRepository.kt
@@ -0,0 +1,20 @@
+package com.eventersapp.gojek_trending.data.repository
+
+import com.eventersapp.gojek_trending.data.dataSource.TrendingRemoteDataSource
+import com.eventersapp.gojek_trending.domain.TrendingModal
+import com.eventersapp.gojek_trending.domain.baseUseCase.ErrorResult
+import com.eventersapp.gojek_trending.domain.trendingRepository.TrendingDomainRepository
+import com.eventersapp.gojek_trending.util.InternetChecker
+import javax.inject.Inject
+
+class TrendingRemoteRepository @Inject constructor(
+    private val trendingRemoteDataSource: TrendingRemoteDataSource,
+    private val connectionDetector: InternetChecker
+) : TrendingDomainRepository {
+    override suspend fun getTrendingRepo(): com.eventersapp.gojek_trending.domain.baseUseCase.Result<TrendingModal> {
+        return if (connectionDetector.isInternetAvailable)
+            trendingRemoteDataSource.trendingRepo()
+        else
+            ErrorResult(message = "No internet connection.")
+    }
+}
